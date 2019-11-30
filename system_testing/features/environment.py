332f9b1e567084244c0adc66eced5372cd31398c
Robson Agapito <robsonagapito@gmail.com>
2019-11-30 00:41:02 -0300

    Fixing READM for TDC POA

--- system_testing/features/environment.py
+++ system_testing/features/environment.py
@@ -2,7 +2,7 @@ from support.driver_qa import DriverQA
 
 def before_all(context):
 	context.driverQA = DriverQA()
-	context.driverQA.configDriver("firefox")
+	context.driverQA.configDriver("chrome")
 
 def after_all(context):
 	context.driverQA.quit()
